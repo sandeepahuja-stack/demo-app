cc2ff7d8e362852d1e1f860d37f341104de4c24b
Sandeep Ahuja <62178420+sandeepahuja-stack@users.noreply.github.com>
2022-11-06 12:20:44 +0530

    Setup/prettier (#2)
    * feat: home page init
    
    * feat/ lint setup
    
    * chore: readme added
    
    Co-authored-by: Sandeep Ahuja <sahuja@mafcarrefour.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import React, { Suspense } from "react";
+
+const Home = React.lazy(() =>
+  import(
+    /* webpackChunkName: "HomePage" */
+    "./pages/Home"
+  )
+);
+
 function App() {
-  
   return (
-   <>
-   Here
-   </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<div>Loading home</div>}>
+            <Home />
+          </Suspense>
+        }
+      />
+    </Routes>
   );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "App";
+import { BrowserRouter } from "react-router-dom";
 
-import ReactDOM from 'react-dom';
-// import App from './App';
-import React from 'react';
+// https://medium.com/geekculture/react-js-architecture-features-folder-structure-design-pattern-70b7b9103f22#7305
 
-ReactDOM.render(<React.StrictMode>
-     Here
-  </React.StrictMode>
-  ,
-  document.getElementById('root')
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
