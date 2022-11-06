a767a9c286314748657d7d9bbbdf6da46a99f661
Sandeep Ahuja <ahuja538@gmail.com>
2022-11-06 15:23:12 +0530

    agent section started

---
+++ src/components/Homepage/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as MOCK_CUSTOMER_LIST_DATA from "./__mock__/data.json";
+const HomePage = () => {
+  console.log(MOCK_CUSTOMER_LIST_DATA);
+  return <>HomePage</>;
+};
+export default HomePage;
